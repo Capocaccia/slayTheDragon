5faeece5d2ddd4eb0b1e15fbbc5171d9a2f1a2bc
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-11-08 11:05:07 -0600

    refactored to work with front end button click and alerts instead of console.logs

--- main.js
+++ main.js
@@ -6,18 +6,21 @@ var damageThisRound = Math.floor(Math.random()*5 + 1)
 
 var totalDamage = 0
 
-while(slaying){
-    if(youHit === 1){
-        console.log("You hit the dragon!")
-        totalDamage += damageThisRound;
-        if(totalDamage >= 4){
-            console.log("You slew the dragon!")
-            slaying = false
-        } else {
-            youHit = Math.floor(Math.random() * 2)
-        }
-    } else {
-        console.log("You were defeated by the dragon!")
-    }
-    slaying = false
+var dragonSlayer = function(){
+    while(slaying){
+      if(youHit === 1){
+          alert("Damage inflicted on the dragon = " + damageThisRound)
+          alert("You hit the dragon with " + damageThisRound + " damage!")
+          totalDamage += damageThisRound;
+          if(totalDamage >= 4){
+              alert("You slew the dragon with " + totalDamage + " damage!")
+              slaying = false
+          } else {
+              youHit = Math.floor(Math.random() * 2)
+          }
+      } else {
+          alert("You were defeated by the dragon!")
+      }
+      slaying = false
+  }
 }
